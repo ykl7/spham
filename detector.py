862f4fb8aed377ea22b653cb199e3af69da8e061
ykl7 <yklal95@gmail.com>
2016-12-18 00:45:10 +0530

    basic naive bayes filter method

--- detector.py
+++ detector.py
@@ -8,8 +8,35 @@ import unicodedata
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold, cross_val_score, train_test_split 
+
 table = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(unichr(i)).startswith('P'))
 stop_words = stopwords.words("english")
+
+def build_naive_bayes_classifier(message_train, message_test, label_train, label_test):
+
+	print label_test
+	pipeline = Pipeline([
+		("vector",CountVectorizer(analyzer=tokenizer)),
+		("tfidf",TfidfTransformer()),
+		("classifier", MultinomialNB())
+	])
+
+	scores = cross_val_score(pipeline, message_train, label_train, cv=10, scoring='accuracy', n_jobs=-1)
+	print scores
+
+	paramaters = { 'tfidf__use_idf' : (True, False)}
+	tuned_classifier = GridSearchCV(pipeline, parameters, n_jobs=-1, scoring='accuracy', cv= StratifiedKFold(label_train, n_folds=5))
+	
+	spam_detector = tuned_classifier.fit(message_train, label_train)
+
+	return spam_detector
 
 def tokenizer(message):
 	message = unicode(message,'utf-8').lower()
